61ac228bc27b929b56514c0d6a9c9573f753e5b2
Kelly Runnels <kellyr@kelly-inspiron660>
2021-11-12 21:45:44 -0500

    Removed all but config and site-lisp files

--- elpa/scanner-0.2/scanner-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from scanner.el  -*- no-byte-compile: t -*-
-(define-package "scanner" "0.2" "Scan documents and images" '((emacs "25.1") (dash "2.12.0")) :keywords '("hardware" "multimedia") :authors '(("Raffael Stocker" . "r.stocker@mnet-mail.de")) :maintainer '("Raffael Stocker" . "r.stocker@mnet-mail.de") :url "https://gitlab.com/rstocker/scanner.git")
