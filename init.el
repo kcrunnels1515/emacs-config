a611c68062778193261d63e36b1ab5e28da13c83
Kelly Runnels <kellyr@kelly-inspiron660>
2021-11-22 09:31:28 -0500

    kjnvksjnvkjn

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(org-roam elfeed-goodies elfeed "openwith" openwith org-present gcmh magit emms scanner mw-thesaurus epc deferred ctable lsp-latex org-bullets vterm smex ivy-rich counsel dashboard which-key use-package general evil-collection doom-themes doom-modeline)))
+   '(elfeed-goodies elfeed "openwith" openwith org-present gcmh magit emms scanner mw-thesaurus epc deferred ctable lsp-latex org-bullets vterm smex ivy-rich counsel dashboard which-key use-package general evil-collection doom-themes doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
